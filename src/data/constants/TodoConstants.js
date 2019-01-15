c1c0ce867521fea987eaaf9c2c7d8d12006b6ead
Akira Hanashiro <felipehanashiro@hotmail.com>
2019-01-15 08:09:23 -0200

    aula-04-02

---
+++ src/data/constants/TodoConstants.js
@@ -0,0 +1,8 @@
+const TodoConstants = {
+    TODO_CREATE: 'TODO_CREATE',
+    TODO_UPDATE: 'TODO_UPDATE',
+    TODO_REMOVE: 'TODO_REMOVE',
+    TODO_CLEAR: 'TODO_CLEAR'
+}
+
+export default TodoConstants;
